43d4b2a5431514379a676d25f7d1aed6e3d4edb4
ömer <omerbaykoca@gmail.com>
2024-08-19 00:36:17 +0300

    Proje dosyası ekle.

---
+++ YılanOyunu/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace YılanOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pnlMain = new Panel();
+            lblSkor = new Label();
+            lbl = new Label();
+            tmrOyun = new System.Windows.Forms.Timer(components);
+            lblOncekiSkor = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            pnlMain.BackColor = Color.Black;
+            pnlMain.Location = new Point(12, 61);
+            pnlMain.Name = "pnlMain";
+            pnlMain.Size = new Size(600, 600);
+            pnlMain.TabIndex = 0;
+            pnlMain.Paint += pnlMain_Paint;
+            // 
+            // lblSkor
+            // 
+            lblSkor.AutoSize = true;
+            lblSkor.BackColor = Color.Transparent;
+            lblSkor.Font = new Font("Courier New", 16F);
+            lblSkor.ForeColor = Color.AliceBlue;
+            lblSkor.Location = new Point(550, 16);
+            lblSkor.Name = "lblSkor";
+            lblSkor.Size = new Size(61, 30);
+            lblSkor.TabIndex = 1;
+            lblSkor.Text = "000";
+            // 
+            // lbl
+            // 
+            lbl.AutoSize = true;
+            lbl.BackColor = Color.Transparent;
+            lbl.Font = new Font("Segoe UI", 16F);
+            lbl.ForeColor = Color.AliceBlue;
+            lbl.Location = new Point(475, 9);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(82, 37);
+            lbl.TabIndex = 2;
+            lbl.Text = "Skor :";
+            // 
+            // tmrOyun
+            // 
+            tmrOyun.Interval = 500;
+            tmrOyun.Tick += tmrOyun_Tick;
+            // 
+            // lblOncekiSkor
+            // 
+            lblOncekiSkor.AutoSize = true;
+            lblOncekiSkor.BackColor = Color.Transparent;
+            lblOncekiSkor.Font = new Font("Courier New", 16F);
+            lblOncekiSkor.ForeColor = Color.AliceBlue;
+            lblOncekiSkor.Location = new Point(233, 16);
+            lblOncekiSkor.Name = "lblOncekiSkor";
+            lblOncekiSkor.Size = new Size(61, 30);
+            lblOncekiSkor.TabIndex = 3;
+            lblOncekiSkor.Text = "000";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.ForeColor = Color.AliceBlue;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(227, 37);
+            label1.TabIndex = 4;
+            label1.Text = "Önceki Skorunuz :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.Chartreuse;
+            label2.Location = new Point(12, 674);
+            label2.Name = "label2";
+            label2.Size = new Size(497, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Herhangi yön tuşuna basarak oyunu başlatın ve yön tuşlarıyla kontrol edin.";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.yilan;
+            ClientSize = new Size(629, 731);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lblOncekiSkor);
+            Controls.Add(lbl);
+            Controls.Add(lblSkor);
+            Controls.Add(pnlMain);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Yılan Oyunu";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel pnlMain;
+        private Label lblSkor;
+        private Label lbl;
+        private System.Windows.Forms.Timer tmrOyun;
+        private Label lblOncekiSkor;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ YılanOyunu/Form1.cs
@@ -0,0 +1,128 @@
+
+
+namespace YılanOyunu
+{
+    public partial class Form1 : Form
+    {
+        List<Point> yilan;
+        int bolumX, bolumY, bolumYukseklik, bolumGenislik, yonX, yonY, skor;
+        Point yem;
+        Random rndmYem = new Random();
+        bool yondegistir;
+        public Form1()
+        {
+            InitializeComponent();
+            OyunuBaslat();
+
+        }
+
+        private void OyunuBaslat()
+        {
+            tmrOyun.Interval = 500;
+            skor = 0;
+            lblSkor.Text = skor.ToString("000");
+            bolumX = 20;
+            bolumY = 20;
+            int baslangicX = bolumX / 2;
+            int baslangicY = bolumY / 2;
+
+            bolumYukseklik = pnlMain.Height / bolumY;
+            bolumGenislik = pnlMain.Width / bolumX;
+            yilan = new List<Point>(bolumGenislik * bolumX / 2)
+            {
+                new Point(baslangicX, baslangicY),
+                new Point(baslangicX-1, baslangicY),
+                new Point(baslangicX-2, baslangicY)
+
+            };
+            yonX = +1;
+            yonY = 0;
+            yemUret();
+        }
+
+        private void yemUret()
+        {
+            do
+            {
+                yem = new Point(rndmYem.Next(bolumX), rndmYem.Next(bolumY));
+            } while (yilan.Any(p => p.X == yem.X && p.Y == yem.Y));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlMain_Paint(object sender, PaintEventArgs e)
+        {
+            bool basi = true;
+            foreach (Point p in yilan)
+            {
+                panelBoya(e.Graphics, p, basi ? Brushes.PowderBlue : Brushes.Pink);
+                basi = false;
+            }
+            panelBoya(e.Graphics, yem, Brushes.OliveDrab);
+        }
+
+        private void panelBoya(Graphics graphics, Point p, Brush firca)
+        {
+            int x = p.X * bolumGenislik;
+            int y = p.Y * bolumGenislik;
+            graphics.FillRectangle(firca, x, y, bolumGenislik, bolumYukseklik);
+            graphics.DrawRectangle(Pens.Black, x, y, bolumGenislik, bolumYukseklik);
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (yondegistir)
+                return base.ProcessCmdKey(ref msg, keyData);
+            if (keyData == Keys.Up || keyData == Keys.Down || keyData == Keys.Left || keyData == Keys.Right)
+                tmrOyun.Start();
+            int x = 0, y = 0;
+            if (keyData == Keys.Up)
+                y = -1;
+            else if (keyData == Keys.Down)
+                y = 1;
+            else if (keyData == Keys.Left)
+                x = -1;
+            else if (keyData == Keys.Right)
+                x = 1;
+
+            if (yonX != -x && yonY != -y)
+            {
+                yonX = x;
+                yonY = y;
+                yondegistir = true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+
+        }
+        private void tmrOyun_Tick(object sender, EventArgs e)
+        {
+            yondegistir = false;
+            Point yeniBas = new Point(yilan[0].X + yonX, yilan[0].Y + yonY);
+            if (yeniBas.X >= bolumX || yeniBas.X < 0 || yeniBas.Y >= bolumY || yeniBas.Y < 0 || yilan.Any(p => p == yeniBas) && yeniBas != yilan[yilan.Count - 1])
+            {
+                tmrOyun.Stop();
+                MessageBox.Show("Oyun Bitti Toplam Skorunuz " + skor);
+                lblOncekiSkor.Text = skor.ToString("000");
+                OyunuBaslat();
+                pnlMain.Refresh();
+                return;
+
+            }
+            yilan.Insert(0, yeniBas);
+
+            if (yeniBas == yem)
+            {
+                tmrOyun.Interval -= 10;
+                skor++;
+                lblSkor.Text = skor.ToString("000");
+                yemUret();
+
+            }
+            else { yilan.RemoveAt(yilan.Count - 1); }
+            pnlMain.Refresh();
+
+        }
+    }
+}
